4bd2838a1c711d78d59888e13d90387b69bf6d19
codeCYJ <67721584+codeCYJ@users.noreply.github.com>
2023-02-15 18:16:17 +0900

    study

---
+++ syntax/template.js
@@ -0,0 +1,5 @@
+const name = "codeCYJ";
+const letter =
+  `Dear ${name}
+  ${3+3} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a ${name} galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the ${name} release of Letraset sheets containing Lorem Ipsum passages, ${name} and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+console.log(letter);
